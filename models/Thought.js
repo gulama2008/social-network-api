712f04d9c86e68fddf5c09f65df686ab8f9dc65d
Siyu Liu <siyu.java.web@gmail.com>
2023-03-28 13:35:03 +1100

    repo init and create models

---
+++ models/Thought.js
@@ -0,0 +1,69 @@
+const { Schema, model } = require("mongoose");
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+        default: Date.now(),
+        get: (date) => {
+
+        }
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// The reactionSchema defines the shape for the reaction subdocument
+const reactionSchema = new mongoose.Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    get: (date) => {},
+  },
+});
+
+// Create a virtual property `reactionCount` that gets the length of the reactions array field on query
+thoughtSchema
+  .virtual("reactionCount")
+  // Getter
+  .get(function () {
+    return this.reactions.length;
+  });
+
+// Initialize our Thought model
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
